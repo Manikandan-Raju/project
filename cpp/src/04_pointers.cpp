24b9407e1148e609f5cc26ef0393fff7842f7717
mani <manikandan.raju@mercedes-benz.com>
2023-03-30 23:32:50 +0530

    pointers cpp

---
+++ cpp/src/04_pointers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int var, *varPoint;
+
+    // Wrong! 
+    // varPoint is an address but var is not
+    // varPoint = var;
+
+    // Wrong!
+    // &var is an address
+    // *varPoint is the value stored in &var
+    // *varPoint = &var;
+
+    // Correct! 
+    // varPoint is an address and so is &var
+    varPoint = &var;
+
+    // Correct!
+    // both *varPoint and var are values
+    *varPoint = var;
+    return 0;
+}
