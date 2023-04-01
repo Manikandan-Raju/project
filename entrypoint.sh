8dd3df3ab5ab19f149a1c81429e11fffe5fe86f6
manikandanraju <itsmanikandanraju@gmail.com>
2023-04-01 19:27:35 +0530

    delete

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 ﻿#!/bin/sh
-echo "HI"
 chmod 755 /workspaces/project/python/py.sh
 sh /workspaces/project/python/py.sh
 python3 /workspaces/project/python/doc.py
--- python/doc.py
+++
@@ -1,3 +0,0 @@
-﻿#! /usr/bin/python3
-print("hi")
-open(r'./workspaces/project/doc.json','w+')
